2ce33ecba511d23eb5051a3a2813392ed43296b9
NEBULA\Asiphile <Asiphile@nebula.co.za>
2025-03-11 10:02:02 +0200

    Added enpoints

---
+++ src/endPoints/endPointList.tsx
@@ -0,0 +1,40 @@
+const Port = "44344";
+//User
+export const LoginEndpoint = "https://localhost:${Port}/api/user/login";
+export const RegisterEndpoint = "https://localhost:${Port}/api/user/register";
+
+//Achievements
+export const GetAllAchievements = "https://localhost:${Port}/api/achievement";
+export const DeleteAchievementById = "https://localhost:${Port}/api/achievement/";
+export const AddAchievement = "https://localhost:${Port}/api/achievement";
+export const UpdateAchievement = "https://localhost:${Port}/api/achievement/";
+
+//Education
+export const GetAllEducation = "https://localhost:${Port}/api/education";
+export const DeleteEducationById = "https://localhost:${Port}/api/education/"; 
+export const AddEducation = "https://localhost:${Port}/api/education";
+export const UpdateEducation = "https://localhost:${Port}/api/education/";
+
+//Hobby
+export const GetAllHobbies = "https://localhost:${Port}/api/hobby";
+export const DeleteHobbyById = "https://localhost:${Port}/api/hobby/";
+export const AddHobby = "https://localhost:${Port}/api/hobby";
+export const UpdateHobby = "https://localhost:${Port}/api/hobby/";
+
+//Experience
+export const GetAllExperience = "https://localhost:${Port}/api/experience";
+export const DeleteExperienceById = "https://localhost:${Port}/api/experience/";
+export const AddExperience = "https://localhost:${Port}/api/experience";
+export const UpdateExperience = "https://localhost:${Port}/api/experience/";
+
+//Project
+export const DeleteProjectById = "https://localhost:${Port}/api/project/";
+export const AddProject = "https://localhost:${Port}/api/project";
+//export const UpdateProject = "https://localhost:${Port}/api/project/";
+export const GetAllProjects = "https://localhost:${Port}/api/project";
+
+//Tags
+//export const GetAllTags = "https://localhost:${Port}/api/tag";
+export const DeleteTagById = "https://localhost:${Port}/api/tag/";
+export const AddTag = "https://localhost:${Port}/api/tag";
+export const UpdateTag = "https://localhost:${Port}/api/tag/";
